88ce01d0d30aace9037ffe89e9b5b36a45aedb23
Daekyu Lee <dagui0@gmail.com>
2025-05-29 19:51:29 +0900

    lambdaspecial

--- src/main/java/HelloJava.java
+++ src/main/java/HelloJava.java
@@ -3,3 +3,8 @@
 //void main() {
 //    System.out.println("Hello, Java!");
 //}
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+    }
+}
